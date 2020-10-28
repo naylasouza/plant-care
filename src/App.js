827657ce741f3961f0488972f9b1b0089395eada
Maria Oliveira <maria.oliveira@accurate.com.br>
2020-10-28 14:12:45 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react"
+import React, { Component } from "react"
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Projeto pronto</h1>
-    </div>
-  );
-}
+class App extends Component() {
+  render(){
+    return (
+      <div className="App">
+        <h1>Projeto pronto</h1>
+      </div>
+    );
+  }
+
+  }
 
 export default App;
