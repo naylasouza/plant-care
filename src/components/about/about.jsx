53abd4c02ef3b4806d008909a85609d362f98e33
Maria Oliveira <maria.oliveira@accurate.com.br>
2020-10-28 15:33:11 -0300

    structure about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import  "./about.css";
+import React, { Component } from "react";
+import "./about.css";
 
-class About extends Component{
-   render(){
-       return(
-           <div>
-               <h1>Eu sou o about, funciona pfvor</h1>
-           </div>
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sobre <br></br> - </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex doloribus reiciendis corrupti aut, fugit quos dolorem maiores! Debitis nesciunt unde laboriosam officiis labore ipsa autem sed velit. Doloribus, tempore nesciunt?
+                Provident ipsam, amet officiis maiores iste assumenda vero rerum, repellat ex excepturi quam nisi facere. Accusantium dolorum molestias aspernatur voluptas neque voluptates velit dolorem adipisci natus vero, quam nam. Sed.
+               Quia odit dolor id nihil, impedit magni adipisci, quos ipsam molestiae ipsa quas. Ex laborum consectetur at sequi, atque nobis amet dolorum quos eius natus vero? Voluptatum et mollitia assumenda.</p>
 
-       )
-       
-   }
+            </div>
+
+        )
+
+    }
 }
 
 
