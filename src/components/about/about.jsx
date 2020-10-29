d5984ccb854aa2523a5a548948821992cac692d9
Maria Oliveira <maria.oliveira@accurate.com.br>
2020-10-29 17:23:57 -0300

    stylizing the about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,9 +4,9 @@ import "./about.css";
 class About extends Component {
     render() {
         return (
-            <div>
-                <h1>Sobre <br></br> - </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex doloribus reiciendis corrupti aut, fugit quos dolorem maiores! Debitis nesciunt unde laboriosam officiis labore ipsa autem sed velit. Doloribus, tempore nesciunt?
+            <div className="about">
+                <h1 className="about_title">Sobre <br></br> - </h1>
+                <p className="about_paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex doloribus reiciendis corrupti aut, fugit quos dolorem maiores! Debitis nesciunt unde laboriosam officiis labore ipsa autem sed velit. Doloribus, tempore nesciunt?
                 Provident ipsam, amet officiis maiores iste assumenda vero rerum, repellat ex excepturi quam nisi facere. Accusantium dolorum molestias aspernatur voluptas neque voluptates velit dolorem adipisci natus vero, quam nam. Sed.
                Quia odit dolor id nihil, impedit magni adipisci, quos ipsam molestiae ipsa quas. Ex laborum consectetur at sequi, atque nobis amet dolorum quos eius natus vero? Voluptatum et mollitia assumenda.</p>
 
