f74400fbae058d64471180f5e761ed39e578d183
Maria Oliveira <maria.oliveira@accurate.com.br>
2020-10-30 13:28:28 -0300

    creation of the card and button component

---
+++ src/components/cardBtn/cardBtn.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./cardBtn.css"
+
+
+class CardBtn extends Component{
+    render(){
+        return(
+            <section>
+                <button>
+                    Aqui é um botão
+                </button>
+            </section>
+        );
+    }
+}
+
+
+export default CardBtn;
