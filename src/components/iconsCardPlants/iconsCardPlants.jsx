3c8c012676662df9c36ad742580b0b67f44d4ac2
Maria Oliveira <maria.oliveira@accurate.com.br>
2020-11-03 14:01:36 -0300

    code identification and comment exclusion

--- src/components/iconsCardPlants/iconsCardPlants.jsx
+++ src/components/iconsCardPlants/iconsCardPlants.jsx
@@ -1,26 +1,21 @@
 import React, { Component } from "react";
-import Icon4 from "../../assets/img/icons/planta4.svg"
-import Icon1 from "../../assets/img/icons/home-person.svg"
-import Icon2 from "../../assets/img/icons/planta1.svg"
-import Icon3 from "../../assets/img/icons/planta2.svg"
-import "./iconsCardPlants.css"
+import Icon4 from "../../assets/img/icons/planta4.svg";
+import Icon1 from "../../assets/img/icons/home-person.svg";
+import Icon2 from "../../assets/img/icons/planta1.svg";
+import Icon3 from "../../assets/img/icons/planta2.svg";
+import "./iconsCardPlants.css";
 
-
-
-
-
-class IconsCardPlants extends Component{
-    render(){
-        return(
-            <div className="icons" id="icons">
-                <img src={Icon4} alt=""/>
-                <img src={Icon2} alt=""/>
-                <img src={Icon1} alt=""/>
-                <img src={Icon3} alt=""/>
-            </div>
-        );
-    }
+class IconsCardPlants extends Component {
+  render() {
+    return (
+      <div className="icons">
+        <img src={Icon4} alt="" />
+        <img src={Icon2} alt="" />
+        <img src={Icon1} alt="" />
+        <img src={Icon3} alt="" />
+      </div>
+    );
+  }
 }
-
 
 export default IconsCardPlants;
