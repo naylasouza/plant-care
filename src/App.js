06b7385ed53f4a28dc3f0c1c395ae6f7f6a6246e
naylasouza <naylasouzariibeiro@gmail.com>
2020-11-05 14:13:29 -0300

    Changes in card structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import CardPlants from "./components/card-plants/card-plants";
 import Footer from "./components/footer/footer";
 import IconsCardPlants from "./components/iconsCardPlants/iconsCardPlants";
 import Menu from "./components/navbar/navbar";
+import planta1 from "../../assets/img/cards_plants/bunny-ears-cacti.jpg";
+import plant2 from "../../assets/img/cards_plants/calathea-triostar.jpg";
+import plant3 from "../../assets/img/cards_plants/ficus-lyrata.jpg";
+import plant4 from "../../assets/img/cards_plants/lucky-bamboo.jpg";
+import plant5 from "../../assets/img/cards_plants/monstera-deliciosa.png";
+import plant6 from "../../assets/img/cards_plants/pilea-peperomioides.jpg";
+import plant7 from "../../assets/img/cards_plants/ponytail-palm.jpg";
+import plant8 from "../../assets/img/cards_plants/succulent-bowl-large.jpg";
+import plant9 from "../../assets/img/cards_plants/euphorbia-eritrea.jpg";
+
+
+
 
 
 class App extends Component {
@@ -12,7 +24,14 @@ class App extends Component {
       <section className="teste">
         <Menu />
         <About />
+        <section className="card-container" >
+        <CardPlants image={planta1} />
         <CardPlants />
+        <CardPlants />
+        <CardPlants />
+        </section>
+        
+
         <IconsCardPlants/>
         <Footer/>
       </section>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import Logo from "../../assets/img/logo.png";
+import styled from 'styled-components';
 import "./navbar.css"
 
+const ul = styled.ul`
+@media (max-width: 500px){
+    display:none;
+    flex-flow: column nowrap;
+    background-color: red;
+    position:fixed;
+    top:0;
+    height: 300px;
+    padding-top: 3.5rem;
 
+    li{
+        color: white
+    }
+}
+`
 class Menu extends Component {
     render(){
         return (
