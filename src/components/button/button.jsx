b36fb8fbeae204c162fc2c166baab32c7c5a9cb7
naylasouza <naylasouzariibeiro@gmail.com>
2020-11-05 16:46:38 -0300

    teste

---
+++ src/components/button/button.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Btn extends Component {
+    render(){
+        return(
+            <div>
+            <a>teeeste</a>
+            </div>
+        )
+    }
+
+}
+
+export default Btn
--- src/components/card-plants/card-plants.jsx
+++ src/components/card-plants/card-plants.jsx
@@ -1,29 +1,38 @@
 
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
-
-// import "./card-plants.css";
+ import "./card-plants.css";
 
 
 const CardPlants = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
+    flex-direction: row;
     background-color: white;
-    max-width: 100%;
+    /* max-width: 100%; */
     width: 24%;  
     border: solid white 1px;
     margin: 1%;
     border-radius: 10%;
-    padding-bottom: 3%; 
+    padding-bottom: 34%; 
     background-repeat: no-repeat;
     background-color: ${props => `${props.background}`};
     background-image: ${props => `url(${props.image})`};
-`
 
-export default CardPlants;
- 
+    
+`
+export const Vermais = styled.div`
+border: 30px;
+    background: #28DF99;
+    padding: 5%;
+    width: 43%;
+    display: flex;
+    justify-content: center;
+    border-radius: 10%;`
+
+export default CardPlants
 // class CardPlants extends Component {
 //   render() {
 //     return (
@@ -31,7 +40,7 @@ export default CardPlants;
         
 //           <div className="card-1">
 //             <div className="card-item">
-//               <img className="card-item--img" src={plant1} alt="aaaa" />
+//              
 //               <h3 className="card-item--title">bunny-ears-cacti</h3>
 //               <a
 //                 className="btn"
