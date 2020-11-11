06cac6fa2a4933a113d7ade734fd6615c183d84c
Maria Oliveira <maria.oliveira@accurate.com.br>
2020-11-11 14:29:36 -0300

    ajusts

--- src/components/cardPlants/cardPlants.jsx
+++ src/components/cardPlants/cardPlants.jsx
@@ -1,9 +1,5 @@
-
-import React, { Component } from 'react';
-import styled from 'styled-components';
-
- 
-
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const CardPlants = styled.div`
     display: flex;
@@ -19,18 +15,16 @@ const CardPlants = styled.div`
     padding-bottom: 4%;
 
     
-    /* background-color: ${props => `${props.background}`};  */
-`
+    /* background-color: ${(props) => `${props.background}`};  */
+`;
 export const VerMais = styled.div`
-    border: 30px;
-    background: #28DF99;
-    padding: 5%;
-    width: 43%;
-    display: flex;
-    justify-content: center;
-    border-radius: 10%;`
-    
-    
+  border: 30px;
+  background: #28df99;
+  padding: 5%;
+  width: 43%;
+  display: flex;
+  justify-content: center;
+  border-radius: 10%;
+`;
 
 export default CardPlants;
-
